179f3a6e021866b33b7a5b5204c528f750838a07
Mike3987 <157983921+Mike3987@users.noreply.github.com>
2026-02-02 19:24:54 -0800

    Update binary_sensor.py

--- components/roode/binary_sensor.py
+++ components/roode/binary_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-import esphome.final_validate as fv
 from esphome.const import (
     CONF_ID,
     CONF_DEVICE_CLASS,
@@ -14,40 +13,36 @@ DEPENDENCIES = ["roode"]
 CONF_PRESENCE = "presence_sensor"
 TYPES = [CONF_PRESENCE]
 
-CONFIG_SCHEMA = cv.Schema(
+# Create a custom binary sensor class for the presence sensor
+RoodePresenceBinarySensor = cg.class_(
+    "RoodePresenceBinarySensor", binary_sensor.BinarySensor, cg.Component
+)
+
+# Use the new schema generator instead of the removed BINARY_SENSOR_SCHEMA
+PRESENCE_BINARY_SENSOR_SCHEMA = binary_sensor.binary_sensor_schema(
+    RoodePresenceBinarySensor
+).extend(
     {
-        cv.GenerateID(CONF_ROODE_ID): cv.use_id(Roode),
-        cv.Optional(CONF_PRESENCE): binary_sensor.BINARY_SENSOR_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
-            }
-        ),
+        cv.GenerateID(CONF_ID): cv.declare_id(RoodePresenceBinarySensor),
+        cv.Optional(CONF_DEVICE_CLASS): cv.enum(binary_sensor.BINARY_SENSOR_DEVICE_CLASSES),
     }
 )
 
-
-# def validate_can_use_presence(value):
-#     main = fv.full_config.get()["roode"][0]
-#     presence_sensor = main.get(CONF_USE_PRESENCE)
-#     print(presence_sensor)
-#     if presence_sensor == False:
-#         raise cv.Invalid("Presence sensor is not enabled")
-#     else:
-#         return presence_sensor
-
-
-# FINAL_VALIDATE_SCHEMA = cv.Schema(
-#     {cv.Optional(CONF_PRESENCE): validate_can_use_presence}, extra=cv.ALLOW_EXTRA
-# )
-
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_ROODE_ID): cv.use_id(Roode),
+        cv.Optional(CONF_PRESENCE): PRESENCE_BINARY_SENSOR_SCHEMA,
+    }
+)
 
 async def setup_conf(config, key, hub):
     if key in config:
         conf = config[key]
         sens = cg.new_Pvariable(conf[CONF_ID])
+        # Register as a binary sensor
         await binary_sensor.register_binary_sensor(sens, conf)
+        # Attach to the hub using setter
         cg.add(getattr(hub, f"set_{key}_binary_sensor")(sens))
-
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_ROODE_ID])
